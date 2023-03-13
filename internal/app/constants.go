7f09624bac76be47750a308cc425e19587fc2894
Alexander Churkin <alex.asutp@gmail.com>
2023-03-13 16:21:50 +0300

    fix: mistake in name

--- internal/app/constants.go
+++ internal/app/constants.go
@@ -2,7 +2,7 @@ package app
 
 const (
 	// Guage название типа метрики с плавающей запятой
-	Guage MetricType = "guage"
+	Guage MetricType = "gauge"
 	// Counter название типа метрики счетчика
 	Counter MetricType = "counter"
 )
