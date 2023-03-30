fa1b461db58268e4357a6b3a67ea5128b74d23f3
Alexander Churkin <alex.asutp@gmail.com>
2023-03-30 17:07:40 +0300

    fix: intervals as duration

--- internal/config/interface.go
+++ internal/config/interface.go
@@ -1,9 +1,11 @@
 package config
 
+import "time"
+
 type AgentConfig struct {
-	Addr           string `env:"ADDRESS" envDefault:"127.0.0.1:8080"`
-	ReportInterval int    `env:"REPORT_INTERVAL" envDefault:"10"`
-	PoolInterval   int    `env:"POLL_INTERVAL" envDefault:"2"`
+	Addr           string        `env:"ADDRESS" envDefault:"127.0.0.1:8080"`
+	ReportInterval time.Duration `env:"REPORT_INTERVAL" envDefault:"10s"`
+	PoolInterval   time.Duration `env:"POLL_INTERVAL" envDefault:"2s"`
 }
 
 type ServerConfig struct {
