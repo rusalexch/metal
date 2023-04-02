d9b893d9cf6d960b1750fdf95a66ad518faa7c3c
Alexander Churkin <alex.asutp@gmail.com>
2023-04-02 08:24:14 +0300

    feat: added file store config

--- internal/config/interface.go
+++ internal/config/interface.go
@@ -9,5 +9,8 @@ type AgentConfig struct {
 }
 
 type ServerConfig struct {
-	Addr string `env:"ADDRESS" envDefault:"127.0.0.1:8080"`
+	Addr          string        `env:"ADDRESS" envDefault:"127.0.0.1:8080"`
+	StoreInterval time.Duration `env:"STORE_INTERVAL" envDefault:"300s"`
+	StoreFile     string        `env:"STORE_FILE" envDefault:"/tmp/devops-metrics-db.json"`
+	Restore       bool          `env:"RESTORE" envDefault:"true"`
 }
