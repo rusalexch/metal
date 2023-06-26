2609b7befe535a231aa298278e798272d5c79d7a
Alexander Churkin <alex.asutp@gmail.com>
2023-06-26 15:14:02 +0300

    ref: updated gdoc server package

--- internal/server/types.go
+++ internal/server/types.go
@@ -4,10 +4,10 @@ import (
 	"github.com/rusalexch/metal/internal/handlers"
 )
 
-// Server структура сервера
+// Server - структура сервера
 type Server struct {
-	// addr адрес сервера
+	// addr - адрес сервера
 	addr string
-	// handler указатель на Хендлеры
+	// handler - указатель на Хендлеры
 	handler *handlers.Handlers
 }
