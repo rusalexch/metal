6cf8586d28143ce1ec091124fd09e9b8252b9f1e
Alexander Churkin <alex.asutp@gmail.com>
2023-06-26 16:01:02 +0300

    ref: updated gdoc utils package

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -15,18 +15,22 @@ func IsSameEmpty(s []string) bool {
 	return res
 }
 
+// StrToFloat64 - преобразование строки в float64
 func StrToFloat64(v string) (float64, error) {
 	return strconv.ParseFloat(v, 64)
 }
 
+// StrToInt64 - преобразование строки в int64
 func StrToInt64(v string) (int64, error) {
 	return strconv.ParseInt(v, 10, 64)
 }
 
+// Float64ToStr - преобразование float64 в строку
 func Float64ToStr(v float64) string {
 	return strconv.FormatFloat(v, 'f', -1, 64)
 }
 
+// Int64ToStr - преобразование int64 в строку
 func Int64ToStr(v int64) string {
 	return strconv.FormatInt(v, 10)
 }
